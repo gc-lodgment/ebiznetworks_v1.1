8e9b2f8ee690197b93b730fa9c530263d868f9b6
vxggangxv <vxggangxv@naver.com>
2019-12-27 18:17:37 +0900

    new

---
+++ static/js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  /*제보하기 팝업창*/
+  $("#compliance").on('click', function() {
+    window.open('http://design.autopay.co.kr/joon/ebiznetworks/compliance.html','제보하기','width=740px,height=785px,scrollbars=no');
+  });
+});
