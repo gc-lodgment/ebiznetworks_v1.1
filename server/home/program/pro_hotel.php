1c0551026fd525da20e4b2657d37caa6da7b2aeb
vxggangxv <vxggangxv@naver.com>
2020-04-01 17:56:38 +0900

    edit home/program

---
+++ server/home/program/pro_hotel.php
@@ -0,0 +1,287 @@
+<div id="left">
+	<ul>
+		<li class="ttl">사업소개</li>
+		<li class="eng">BUSINESS</li>
+	</ul>
+	<!--좌측메뉴-->
+	<ul id="left_menu">
+		<li><a href="<?=base_doc?>/program/?dd=<?=$ebiz_control->XOREncode('pro_tools')?>" id="pro_tools">소프트웨어</a></li>
+		<li><a href="<?=base_doc?>/program/?dd=<?=$ebiz_control->XOREncode('pro_hotel')?>" id="pro_hotel">호텔위탁운영</a></li>
+		<!--<li><a href="<?=base_doc?>/program/?dd=<?=$ebiz_control->XOREncode('pro_game')?>" id="pro_game">게임서비스</a></li>-->
+		<li><a href="<?=base_doc?>/program/?dd=<?=$ebiz_control->XOREncode('pro_shop')?>" id="pro_shop">쇼핑몰</a></li>
+		<li><a href="<?=base_doc?>/program/?dd=<?=$ebiz_control->XOREncode('pro_kt')?>" id="pro_kt">KT대리점 사업</a></li>
+		<li><a href="<?=base_doc?>/program/?dd=<?=$ebiz_control->XOREncode('pro_esset')?>" id="pro_esset">부동산 임대업</a></li>
+		<li><a href="<?=base_doc?>/program/?dd=<?=$ebiz_control->XOREncode('pro_go')?>" id="pro_go">여행ㆍ레져사업</a></li>
+	</ul>
+	<!--좌측메뉴 끝-->
+</div>
+<div id="contents">
+	<dl id="contents_ttl">
+		<dt class="ttl_22">호텔 위탁운영 전문가</dt>
+		<dd class="ttl_30">수익을 창출하는 <span class="orange">맞춤 위탁운영관리</span>서비스</dd>
+	</dl>
+	<!-- <h2><img src="<?=base_img?>/title_hotel.jpg" border="0" alt="호텔위탁운영" /></h2> -->
+	<div id="operationWrapper" class="operation-wrapper">
+		<ul class="operation-tab-list cf">
+			<li class="tab active"><a href="#operationPanel">호텔/리조트 위탁운영</a></li>
+			<li class="tab"><a href="#operationHistoryPanel">호텔/리조트 위탁운영 이력</a></li>
+		</ul>
+		<div class="operation-tabpanel-list">
+			<div id="operationPanel" class="tabpanel operation-panel active">
+				<div class="hotel-wrapper">
+					<img src="<?=base_img?>/program/img_01.jpg" alt="호텔운영설명" class="img-1">
+				</div>
+				<!-- /.hotel-wrapper -->
+				<div class="service-wrapper">
+					<div class="service-title comm-title">
+						<p class="title">특화된 위탁운영 서비스</p>
+						<p class="st">
+							특화된 운영시스템을 통해 전문성을 갖춘 우수 인재를 적재적소에 배치하고<br>
+							<span class="c-blue">호텔 경영과 운영을 체계화하여 수익을 만들어 내는 호텔/리조트 전문 위탁운영회사</span>입니다.
+						</p>
+					</div>
+					<img src="<?=base_img?>/program/img_02.jpg" alt="특화된 위탁운영 서비스" class="img-2">
+				</div>
+				<!-- /.service-wrapper -->
+				<div class="manage-wrapper">
+					<div class="manage-content">
+						<p class="manage-title">보유한 시설관리 인력</p>
+						<div class="manage-text-wrapper">
+							<div class="manage-tbl-wrapper">
+								<table class="manage-tbl">
+									<colgroup>
+										<col style="width: 160px">
+										<col>
+									</colgroup>
+									<tbody>
+										<tr>
+											<th>직접 보유 인력</th>
+											<td>소방안전관리기술자, 전기안전관리기술자, 가스안전관리기술자 등</td>
+										</tr>
+										<tr>
+											<th>하도급 보유 인력</th>
+											<td>
+												남부전기, 현대엘리베이터, 하나로 MCM(방역),<br>
+												청광(음식물, 폐기물 수거) 등 약 30개 업체와 업무제휴
+											</td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+					<div class="manage-content">
+						<p class="manage-title">전문인력배치 시설간 공조</p>
+						<div class="manage-text-wrapper">
+							<div class="manage-tbl-wrapper">
+								<table class="manage-tbl">
+									<tbody>
+										<tr>
+											<td>
+												<span class="dot">&middot;</span> 영업/마케팅, 시설관리, 온라인 광고, 법무/세무/회계, IT개발자, 디자인 인력 고정배치<br>
+												<span class="dot">&middot;</span> 지역시설간의 공동협업으로 긴급 대응 및 시너지 효과 창출<br>
+												<span class="dot">&middot;</span> 시설의 경영진단, 개선안 도출 방안 및 공조 효과 창출
+											</td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- /.manage-wrapper -->
+				<div class="operation-history-panel">
+					<div id="operationHistoryWrapper" class="history-wrapper">
+						<div class="history-title comm-title">
+							<p class="title">호텔/리조트 위탁관리 이력</p>
+							<p class="st">
+								다수의 시설 보유, 건축과 관리운영경험을 바탕으로 시설의 자체 관리 능력을 향상시키고,<br>
+								운영사업장의 높은 투자수익률을 실현하는 경영능력을 인정 받고 있습니다.
+							</p>
+						</div>
+						<div class="history-card">
+							<ul class="card-list cf">
+								<li class="card ocean-card">
+									<div class="default cf">
+										<figure class="card-picture">
+											<img src="<?=base_img?>/program/img_ocean.jpg" alt="오션투유리조트">
+										</figure>
+										<div class="card-text">
+											<span class="state-badge ing">운영중</span>
+											<div class="text-wrapper">
+												<p class="title">오션투유리조트</p>
+												<p class="text">250객실 &middot; 강원 고성 죽왕면</p>
+											</div>
+										</div>
+									</div>
+									<div class="hover">
+										<figure class="card-picture">
+											<img src="<?=base_img?>/program/logo_ocean.png" alt="오션투유" class="logo">
+										</figure>
+										<div class="card-title">오션투유리조트</div>
+										<div class="card-text">
+											<p class="term"><span class="dot">▶</span> 운영기간: 2017. 03 ~ 현재</p>
+											<p class="text">
+												&middot; 객실: 250개 (전 객실 76㎡)<br>
+												&middot; 시설: 연회장, 비즈니스룸, 바베큐장, 횟집,<br>
+												<span class="space"></span>뷔페, 커피숍, 노래방 등<br>
+												&middot; 소재: 강원도고성군 죽왕면삼포해변길 9
+											</p>
+										</div>
+										<div class="card-actions">
+											<a href="http://ocean2you.co.kr/" target="_blank" rel="noopener noreferrer" class="home-btn">홈페이지</a>
+										</div>
+									</div>
+                                </li>
+                                <li class="card seabay-card">
+									<div class="default cf">
+										<figure class="card-picture">
+											<img src="<?=base_img?>/program/img_seabay.jpg" alt="강릉씨베이호텔">
+										</figure>
+										<div class="card-text">
+											<span class="state-badge ing">운영중</span>
+											<div class="text-wrapper">
+												<p class="title">강릉씨베이호텔</p>
+												<p class="text">340객실 &middot; 강원 강릉 주문진</p>
+											</div>
+										</div>
+									</div>
+									<div class="hover">
+										<figure class="card-picture">
+											<img src="<?=base_img?>/program/logo_seabay.png" alt="강릉씨베이호텔" class="logo">
+										</figure>
+										<div class="card-title">강릉씨베이호텔</div>
+										<div class="card-text">
+											<p class="term"><span class="dot">▶</span> 운영기간: 2019. 07 ~ 현재</p>
+											<p class="text">
+												&middot; 객실: 340 객실 (21㎡, 24㎡, 32.7㎡, 50㎡)<br>
+												&middot; 시설: 레스토랑, 바, 연회장, 비즈니스코너, 풀장 등<br>
+												&middot; 소재: 강원도 강릉시 주문진읍 주문로 59<br>
+											</p>
+										</div>
+										<div class="card-actions">
+											<a href="http://seabay.co.kr/" target="_blank" rel="noopener noreferrer" class="home-btn">홈페이지</a>
+										</div>
+									</div>
+								</li>
+                                <li class="card rgb-card">
+                                    <div class="default cf">
+                                        <figure class="card-picture">
+                                            <img src="<?=base_img?>/program/img_rgb.jpg" alt="알지비스카이호텔">
+                                        </figure>
+                                        <div class="card-text">
+                                            <span class="state-badge img">예정</span>
+                                            <div class="text-wrapper">
+                                                <p class="title">알지비스카이호텔</p>
+                                                <p class="text">457객실 &middot; 인천광역시 중구 중산동</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="hover">
+                                        <figure class="card-picture">
+                                            <!-- <img src="<?=base_img?>/program/logo_ramadapt.png" alt="알지비스카이호텔" class="logo"> -->
+                                        </figure>
+                                        <div class="card-title">알지비스카이호텔</div>
+                                        <div class="card-text">
+                                            <p class="term"><span class="dot">▶</span> 운영기간: 2020. 05. 01 ~ 운영예정</p>
+                                            <p class="text">
+                                                &middot; 객실: 457개 객실<br>
+                                                &middot; 시설: 연회장, 비즈니스룸, 레스토랑, 바/라운지 등<br>
+                                                &middot; 소재: 인천광역시 중구 중산동 1952-4<br>
+                                            </p>
+                                        </div>
+                                        <div class="card-actions">
+                                            <!-- <a href="http://ramadapt.co.kr/" target="_blank" rel="noopener noreferrer" class="home-btn">홈페이지</a> -->
+                                            <a href="javascript:;" class="home-btn">준비중</a>
+                                        </div>
+                                    </div>
+                                </li>
+								<li class="card ramadapt-card">
+									<div class="default cf">
+										<figure class="card-picture">
+											<img src="<?=base_img?>/program/img_ramadapt.jpg" alt="">
+										</figure>
+										<div class="card-text">
+                                            <span class="state-badge end">운영이력</span>
+											<div class="text-wrapper">
+												<p class="title">라마다평택호텔앙코르</p>
+												<p class="text">302객실 &middot; 경기 평택시 포승읍</p>
+											</div>
+										</div>
+									</div>
+									<div class="hover">
+										<figure class="card-picture">
+											<img src="<?=base_img?>/program/logo_ramadapt.png" alt="라마다평택호텔앙코르" class="logo">
+										</figure>
+										<div class="card-title">라마다평택호텔앙코르</div>
+										<div class="card-text">
+                                            <p class="term"><span class="dot">▶</span> 운영기간: 2018. 11 ~ 2020. 03. 31</p>
+											<p class="text">
+												&middot; 객실: 302개 객실 (23㎡, 24㎡, 46㎡)<br>
+												&middot; 시설: 레스토랑, 비즈니스룸, 휘트니스, 야외테라스,<br>
+												<span class="space"></span>세탁실 등<br>
+												&middot; 소재: 경기도 평택시 포승읍 평택항로 184번길<br>
+											</p>
+										</div>
+										<div class="card-actions">
+											<a href="http://ramadapt.co.kr/" target="_blank" rel="noopener noreferrer" class="home-btn">홈페이지</a>
+										</div>
+									</div>
+								</li>
+								<li class="card bs-card">
+									<div class="default cf">
+										<figure class="card-picture">
+											<img src="<?=base_img?>/program/img_bs.jpg" alt="BS콘도 도고온천 호텔 앤 리조트">
+										</figure>
+										<div class="card-text">
+											<span class="state-badge end">운영이력</span>
+											<div class="text-wrapper">
+												<p class="title">BS콘도 도고온천 호텔 앤 리조트</p>
+												<p class="text">200객실 &middot; 충남 아산시 도고면</p>
+											</div>
+										</div>
+									</div>
+									<div class="hover">
+										<figure class="card-picture">
+											<img src="<?=base_img?>/program/logo_bs.png" alt="BS콘도 도고온천 호텔 앤 리조트" class="logo">
+										</figure>
+										<div class="card-title bs">BS콘도<br> 도고온천 호텔 앤 리조트</div>
+										<div class="card-text">
+											<p class="term"><span class="dot">▶</span> 운영기간: 2018. 01 ~ 2019.07</p>
+											<p class="text">
+												&middot; 객실: 200 객실 (89㎡, 135㎡)<br>
+												&middot; 시설: 연회장, 비즈니스룸, 한식당, 대식당,<br>
+												<span class="space"></span>바베큐장 노래방, 편의점, 오락실 등<br>
+												&middot; 소재: 충청남도 아산시 도고면 기곡로62번길 6-8<br>
+											</p>
+										</div>
+										<div class="card-actions">
+											<a href="http://bscondo.co.kr/" target="_blank" rel="noopener noreferrer" class="home-btn">홈페이지</a>
+										</div>
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<!-- /.operation-history-panel -->
+			</div>
+			<div id="operationHistoryPanel" class="tabpanel">
+				
+			</div>
+		</div>
+	</div>
+	<script>
+		$('#operationWrapper .operation-tab-list a').on('click', function (e) {
+			e.preventDefault();
+			var thisIndex = $(this).closest('li').index();
+			// console.log(thisIndex);
+			var elHistoryWrapper = $('#operationWrapper .operation-history-panel').detach();
+
+			$($(this).attr('href')).append(elHistoryWrapper);
+
+			$(this).closest('li').addClass('active').siblings().removeClass('active');
+			$($(this).attr('href')).addClass('active').siblings().removeClass('active');
+		});
+	</script>	
+</div>
